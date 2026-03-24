19a8e8fdb6efdde819297a57b9934bb42e07bcee
sherishadevi-rgb <sherishadevi@gmail.com>
2026-03-24 09:02:20 +0530

    UC3: Track Unique Bogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        List<String> passengerBogies = new ArrayList<>();
+        Set<String> bogieIds = new HashSet<>();
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+        bogieIds.add("BG103");
+        bogieIds.add("BG102");
 
-        System.out.println("After adding bogies: " + passengerBogies);
-
-        passengerBogies.remove("AC Chair");
-
-        System.out.println("After removing AC Chair: " + passengerBogies);
-
-        System.out.println("Contains Sleeper: " + passengerBogies.contains("Sleeper"));
+        System.out.println("Unique Bogie IDs: " + bogieIds);
     }
 }
-
