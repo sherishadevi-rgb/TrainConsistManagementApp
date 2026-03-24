d48265b1de1e7cc4dce0fd089c249bed4a9431f1
sherishadevi-rgb <sherishadevi@gmail.com>
2026-03-24 08:57:37 +0530

    UC2: Add Passenger Bogies to Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -4,7 +4,20 @@ import java.util.List;
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        List<String> consist = new ArrayList<>();
-        System.out.println("Initial bogie count: " + consist.size());
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("After adding bogies: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("After removing AC Chair: " + passengerBogies);
+
+        System.out.println("Contains Sleeper: " + passengerBogies.contains("Sleeper"));
     }
 }
+
